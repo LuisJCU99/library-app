4566077f677e4675bb08343b45b8aef5b8ceaf2d
Luis <luisJCU99@gmail.com>
2022-08-18 15:43:56 +0200

    Bootstrap. Home, Genres component. First Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { GenresComponent } from './genres/genres.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes:Routes=[
+  {path: 'home', component: HomeComponent},
+  {path: 'genres', component: GenresComponent},
+  
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenresComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
