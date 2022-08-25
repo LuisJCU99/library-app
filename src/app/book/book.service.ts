28f20501bfb82df10d31efc7af3d30f0377c41a3
Luis <luisJCU99@gmail.com>
2022-08-25 11:48:51 +0200

    Adding cards inside ngFor

--- src/app/book/book.service.ts
+++ src/app/book/book.service.ts
@@ -8,52 +8,34 @@ export class BookService {
     return [
       {
         ID_genre: '1', ISBN: 'Preety', Name: 'Tiwary',
-        Description: 'https://www.gstatic.com/webp/gallery3/1.png'
+        Image: 'https://www.gstatic.com/webp/gallery3/1.png',
+        Description: 'This is a wider card with supporting text below as a natural lead-in to additional content. Thi wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Ths content is a little bit longer.'
+    },
+    {
+        ID_genre: '1', ISBN: 'Preety', Name: 'Tiwary',
+        Image: 'https://www.gstatic.com/webp/gallery3/1.png',
+        Description: 'This is a wider card with supporting text below as a natural lead-in to additional content. Thi wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Ths content is a little bit longer.'
+    },
+    {
+        ID_genre: '1', ISBN: 'Preety', Name: 'Tiwary',
+        Image: 'https://www.gstatic.com/webp/gallery3/1.png',
+        Description: 'This is a wider card with supporting text below as a natural lead-in to additional content. Thi wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Ths content is a little bit longer.'
     },
     {
         ID_genre: '2', ISBN: 'Preety', Name: 'Tiwary',
-        Description: 'https://www.gstatic.com/webp/gallery3/1.png'
+        Image: 'https://www.gstatic.com/webp/gallery3/1.png',
+        Description: 'This is a wider card with supporting text below as a natural lead-in to additional content. Thi wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Ths content is a little bit longer.'
     },
     {
-        ID_genre: '3', ISBN: 'Preety', Name: 'Tiwary',
-        Description: 'https://www.gstatic.com/webp/gallery3/1.png'
+        ID_genre: '2', ISBN: 'Preety', Name: 'Tiwary',
+        Image: 'https://www.gstatic.com/webp/gallery3/1.png',
+        Description: 'This is a wider card with supporting text below as a natural lead-in to additional content. Thi wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Ths content is a little bit longer.'
     },
     {
-        ID_genre: '3', ISBN: 'Preety', Name: 'Tiwary',
-        Description: 'https://www.gstatic.com/webp/gallery3/1.png'
+        ID_genre: '2', ISBN: 'Preety', Name: 'Tiwary',
+        Image: 'https://www.gstatic.com/webp/gallery3/1.png',
+        Description: 'This is a wider card with supporting text below as a natural lead-in to additional content. Thi wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Th wider card with supporting text below as a natural lead-in to additional content. Ths content is a little bit longer.'
     },
-    {
-        ID_genre: '3', ISBN: 'Preety', Name: 'Tiwary',
-        Description: 'https://www.gstatic.com/webp/gallery3/1.png'
-    },
-    {
-        ID_genre: '4', ISBN: 'Preety', Name: 'Tiwary',
-        Description: 'https://www.gstatic.com/webp/gallery3/1.png'
-    },
-    {
-        ID_genre: '5', ISBN: 'Preety', Name: 'Tiwary',
-        Description: 'https://www.gstatic.com/webp/gallery3/1.png'
-    },
-    {
-        ID_genre: '6', ISBN: 'Preety', Name: 'Tiwary',
-        Description: 'https://www.gstatic.com/webp/gallery3/1.png'
-    },
-    {
-        ID_genre: '7', ISBN: 'Preety', Name: 'Tiwary',
-        Description: 'https://www.gstatic.com/webp/gallery3/1.png'
-    },
-    {
-        ID_genre: '8', ISBN: 'Preety', Name: 'Tiwary',
-        Description: 'https://www.gstatic.com/webp/gallery3/1.png'
-    },
-    {
-        ID_genre: '8', ISBN: 'Preety', Name: 'Tiwary',
-        Description: 'https://www.gstatic.com/webp/gallery3/1.png'
-    },
-    {
-        ID_genre: '8', ISBN: 'Preety', Name: 'Tiwary',
-        Description: 'https://www.gstatic.com/webp/gallery3/1.png'
-    }
     ];
   }
 }
